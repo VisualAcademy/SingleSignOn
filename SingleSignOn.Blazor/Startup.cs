ac75b5eaeac1786226165cb1b547d10c049035be
WebCampKR <webcamp@outlook.kr>
2020-01-16 02:29:50 +0900

    Hybrid 방식으로 변경 후 쿠키 정보 중 이름과 이메일을 뷰 페이지 출력하기

--- SingleSignOn.Blazor/Startup.cs
+++ SingleSignOn.Blazor/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -46,6 +47,8 @@ namespace SingleSignOn.Blazor
             });
 
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); // 인증 정보 얻기
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
